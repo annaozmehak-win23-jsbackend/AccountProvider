939d7f76ca0850b5a4ddcbacb51800ffc5410dbf
Anna Özmehak <anna.ozmehak@gmail.com>
2024-05-10 15:49:59 +0200

    completed

---
+++ AccountProvider/Functions/SignUp.cs
@@ -0,0 +1,97 @@
+using AccountProvider.Models;
+using Data.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace AccountProvider.Functions
+{
+    public class SignUp(ILogger<SignUp> logger, UserManager<UserAccount> userManager)
+    {
+        private readonly ILogger<SignUp> _logger = logger;
+        private readonly UserManager<UserAccount> _userManager = userManager;
+
+
+        [Function("SignUp")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+        {
+            string body = null!;
+
+            try
+            {
+                body = await new StreamReader(req.Body).ReadToEndAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"StreamReader :: {ex.Message}");
+            }
+
+            if (body != null)
+            {
+                UserRegistrationRequest urr = null!;
+
+                try
+                {
+                    urr = JsonConvert.DeserializeObject<UserRegistrationRequest>(body)!;
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"JsonConvert.DeserializeObject<UserRegistrationRequest> :: {ex.Message}");
+                }
+
+                if (urr != null && !string.IsNullOrEmpty(urr.Email) && !string.IsNullOrEmpty(urr.Password))
+                {
+                    if (!await _userManager.Users.AnyAsync(x => x.Email == urr.Email))
+                    {
+                        var userAccount = new UserAccount
+                        {
+                            FirstName = urr.Email,
+                            LastName = urr.Password,
+                            Email = urr.Email,
+                            UserName = urr.Email
+                        };
+
+                        try
+                        {
+                            var result = await _userManager.CreateAsync(userAccount, urr.Password);
+                            if (result.Succeeded)
+                            {
+                                // send verification code
+                                //change so it works with my verificationprovider
+                                //om du vill kora det som en HTTP REQUEST - kraver att vi vantar pa svar tillbaka
+                                try
+                                {
+                                    using var http = new HttpClient();
+                                    StringContent content = new StringContent(JsonConvert.SerializeObject(new { Email = userAccount.Email }), Encoding.UTF8, "application/json");
+                                    var response = await http.PostAsync("https://verificationprovider.silicon.azurewebsite.net/api/generate", content);
+                                }
+                                catch (Exception ex)
+                                {
+                                    _logger.LogError($"http.PostAsync :: {ex.Message}");
+                                }
+
+                                return new OkResult();
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            _logger.LogError($"_userManager.CreateAsync :: {ex.Message}");
+                        }
+
+                    }
+                    else
+                    {
+                        return new ConflictResult();
+                    }
+                }
+            }
+
+            return new BadRequestResult();
+        }
+    }
+}
---
+++ AccountProvider/Functions/Verify.cs
@@ -0,0 +1,99 @@
+using AccountProvider.Models;
+using Data.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace AccountProvider.Functions;
+
+public class Verify(ILogger<Verify> logger, UserManager<UserAccount> userManager)
+{
+    private readonly ILogger<Verify> _logger = logger;
+    private readonly UserManager<UserAccount> _userManager = userManager;
+
+
+    [Function("Verify")]
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+    {
+        string body = null!;
+
+        try
+        {
+            body = await new StreamReader(req.Body).ReadToEndAsync();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"StreamReader :: {ex.Message}");
+        }
+
+        if (body != null)
+        {
+            VerificationRequest vr = null!;
+
+            try
+            {
+                vr = JsonConvert.DeserializeObject<VerificationRequest>(body)!;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"JsonConvert.DeserializeObject<VerificationRequest> :: {ex.Message}");
+            }
+
+            if (vr != null && !string.IsNullOrEmpty(vr.Email) && !string.IsNullOrEmpty(vr.VerificationCode))
+            {
+                // verify code using VerificationProvider
+                try
+                {
+                    //dessa tre rader borde sta inuti i en try
+                    using var http = new HttpClient();
+                    StringContent content = new StringContent(JsonConvert.SerializeObject(vr), Encoding.UTF8, "application/json");
+                    //var response = await http.PostAsync("https://verificationprovider.silicon.azurewebsite.net/api/verify", content);
+
+                    //if (response.IsSuccessStatusCode)
+                    //{
+                    //    var userAccount = await _userManager.FindByEmailAsync(vr.Email);
+                    //    if (userAccount != null)
+                    //    {
+                    //        userAccount.EmailConfirmed = true;
+                    //        await _userManager.UpdateAsync(userAccount);
+
+                    //        if (await _userManager.IsEmailConfirmedAsync(userAccount))
+                    //        {
+                    //            return new OkResult();
+                    //        }
+                    //    }
+                    //}
+
+                    //simulated, remove true on real connection like above
+                    if (true)
+                    {
+                        var userAccount = await _userManager.FindByEmailAsync(vr.Email);
+                        if (userAccount != null)
+                        {
+                            userAccount.EmailConfirmed = true;
+                            await _userManager.UpdateAsync(userAccount);
+
+                            if (await _userManager.IsEmailConfirmedAsync(userAccount))
+                            {
+                                return new OkResult();
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"http.PostAsync :: {ex.Message}");
+                }
+
+                
+            }
+        }
+
+        return new UnauthorizedResult();
+       
+    }
+}
